dcbe1d2438dcc1a3c99be91d2babcb3a95b5d9b4
s s <swebhelps@gmail.com>
2022-11-29 01:53:03 +0530

    dev commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\DashboardController@loadDashboard')->name('dashboard');
 
-Route::get('/xmldata', 'App\Http\Controllers\DashboardController@storeXMLReadData')->name('xmldata');
+//Route::get('/xmldata', 'App\Http\Controllers\DashboardController@storeXMLReadData')->name('xmldata');
 
 Route::any('/searchUser', 'App\Http\Controllers\DashboardController@show')->name('searchUser');
 
