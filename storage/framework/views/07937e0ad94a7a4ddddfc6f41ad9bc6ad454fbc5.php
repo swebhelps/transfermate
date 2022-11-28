30979b2cfb340ffc2cb157877f8ac4d542675950
s s <swebhelps@gmail.com>
2022-11-29 01:21:30 +0530

    first comment

--- storage/framework/views/07937e0ad94a7a4ddddfc6f41ad9bc6ad454fbc5.php
+++
@@ -1,8 +0,0 @@
-
-  <script src="<?php echo e(asset('js/core/popper.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/core/bootstrap.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/plugins/perfect-scrollbar.min.js')); ?>"></script>
-  <script src="<?php echo e(asset('js/plugins/smooth-scrollbar.min.js')); ?>"></script>
-
-</body>
-</html><?php /**PATH C:\Apache24\htdocs\blog\resources\views/include/footer.blade.php ENDPATH**/ ?>
