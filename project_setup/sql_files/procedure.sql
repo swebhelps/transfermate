22a13f305180c7f6c54ed725d951473007bf420c
s s <swebhelps@gmail.com>
2022-11-28 14:28:10 +0530

    First commit

--- project_setup/sql_files/procedure.sql
+++ project_setup/sql_files/procedure.sql
@@ -4,7 +4,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `getAuthorDetailsByName`(IN `author_
     NO SQL
 BEGIN
     SET @author_name_str = author_name_str;
-    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id where a.author_name LIKE '%", @author_name_str,"%' group by a.author_id");
+    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id where a.author_name LIKE '%", @author_name_str,"%'");
 
     PREPARE STMT FROM @query;
     EXECUTE STMT;
@@ -17,7 +17,7 @@ DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAuthorDetails`()
     NO SQL
 BEGIN
-    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id  group by a.author_id");
+    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id  ");
 
     PREPARE STMT FROM @query;
     EXECUTE STMT;
