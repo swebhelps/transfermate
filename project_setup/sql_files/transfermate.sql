cd50136e8a592dd18161406114d365e48cc7148a
s s <swebhelps@gmail.com>
2022-11-28 14:28:46 +0530

    First commit

--- project_setup/sql_files/transfermate.sql
+++ project_setup/sql_files/transfermate.sql
@@ -27,7 +27,7 @@ DELIMITER $$
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAuthorDetails` ()  NO SQL
 BEGIN
-    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id  group by a.author_id");
+    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id  ");
 
     PREPARE STMT FROM @query;
     EXECUTE STMT;
@@ -37,7 +37,7 @@ END$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAuthorDetailsByName` (IN `author_name_str` LONGTEXT)  NO SQL
 BEGIN
     SET @author_name_str = author_name_str;
-    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id where a.author_name LIKE '%", @author_name_str,"%' group by a.author_id");
+    SET @query =  CONCAT("SELECT a.author_name,b.book_name FROM `author` as a LEFT JOIN book as b on b.author_id=a.author_id where a.author_name LIKE '%", @author_name_str,"%'");
 
     PREPARE STMT FROM @query;
     EXECUTE STMT;
